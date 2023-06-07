87033bb2f666098dc2c5ded57f61abe099609a1a
gyurika <gyorgykoszegi39@gmail.com>
2023-06-07 17:28:03 +0200

    makes a or b equal to c

---
+++ Java/minimum-flips-to-make-a-or-b-equal-to-c/Solution.java
@@ -0,0 +1,27 @@
+
+public class Solution {
+    public static int minFlips(int a, int b, int c) {
+        int count = 0;
+        for(int i = 31; i >= 0; i--) {
+            int mask = 1 << i;
+            Boolean aBitIsTrue = (a & mask) != 0;
+            Boolean bBitIsTrue = (b & mask) != 0;
+            Boolean cBitIsTrue = (c & mask) != 0;
+
+            if(!cBitIsTrue.equals(aBitIsTrue || bBitIsTrue)) {
+                System.out.println(aBitIsTrue + " " + bBitIsTrue + " " + cBitIsTrue);
+
+                count++;
+
+                if(aBitIsTrue && bBitIsTrue)
+                    count++;
+            }
+        }
+
+        return count;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Solution.minFlips(12, 56, 95));
+    }
+}

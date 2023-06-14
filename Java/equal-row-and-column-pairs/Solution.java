b952f08f91fe766664e3c512c0853b92473f64c0
gyurika <gyorgykoszegi39@gmail.com>
2023-06-14 10:01:19 +0200

    removes unused code

--- Java/equal-row-and-column-pairs/Solution.java
+++ Java/equal-row-and-column-pairs/Solution.java
@@ -11,33 +11,18 @@ public class Solution {
             char[] columnArray = new char[n];
             for(int row = 0; row < n; row++)
                 columnArray[row] = (char) grid[row][column];
-
             String key = String.valueOf(columnArray);
-            System.out.println(key);
             pairs.put(key, pairs.getOrDefault(key, 0) + 1);
         }
 
         for(int row = 0; row < n; row++) {
-
             char[] rowArray = new char[n];
             for(int column = 0; column < n; column++)
                 rowArray[column] = (char) grid[row][column];
-
-            System.out.println(String.valueOf(rowArray));
-
             count += pairs.getOrDefault(String.valueOf(rowArray), 0);
         }
 
         return count;
-    }
-
-    private static int isEqualRowAndColumn(int[][] grid, int n, int row, int column) {
-        for(int i = 0; i < n; i++) {
-            if(grid[row][i] != grid[i][column])
-                return 0;
-        }
-
-        return 1;
     }
 
     public static void main(String[] args) {

3a9348033de11f5d6de1dc6626c076f87ad4b9eb
gyurika <gyorgykoszegi39@gmail.com>
2023-09-14 21:08:37 +0200

    merges two linked lists using recursion

--- Java/merge-two-sorted-lists/Solution.java
+++ Java/merge-two-sorted-lists/Solution.java
@@ -8,21 +8,19 @@ class ListNode {
 
 public class Solution {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-        ListNode result = new ListNode(), list = result;
+        if(list1 == null)
+            return list1;
 
-        while(list1 != null && list2 != null) {
-            if(list1.val <= list2.val) {
-                list.next = list1;
-                list1 = list1.next;
-            } else {
-                list.next = list2;
-                list2 = list2.next;
-            }
-            
-            list = list.next;
+        if(list2 == null)
+            return list2;
+        
+        if(list1.val < list2.val) {
+            list1.next = mergeTwoLists(list1.next, list2);
+            return list1;
+        } else {
+            list2.next = mergeTwoLists(list1, list2.next);
+            return list2;
         }
-        list.next = list1 != null ?  list1: list2;
-        return result.next;
     }
 
     public static void main(String[] args) {

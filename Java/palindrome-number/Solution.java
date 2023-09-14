a936aa54f64dd4f665d4ded172c51ebf754b0e30
gyurika <gyorgykoszegi39@gmail.com>
2023-09-14 14:50:20 +0200

    checks if the number is palindrome - improves runtime

--- Java/palindrome-number/Solution.java
+++ Java/palindrome-number/Solution.java
@@ -1,15 +1,16 @@
 public class Solution {
     
     public boolean isPalindrome(int x) {
-        if(x < 0)
+        if(x < 0 || (x != 0 && x % 10 == 0))
             return false;
-        int num = 0, originalNum = x;
-        while(x > 0) {
+        int num = 0;
+        while(x > num) {
             num = num * 10 + x % 10;
             x /= 10;
         }
 
-        return originalNum == num;    
+        return x == num || x == num / 10;
+        
     }
     public static void main(String[] args) {
         Solution s = new Solution();

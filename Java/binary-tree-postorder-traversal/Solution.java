a57bb6595c30303eda6d3d2cd9bd6d43e8e800af
gyurika <gyorgykoszegi39@gmail.com>
2023-09-18 16:54:03 +0200

    implements postorder traversal of a tree

---
+++ Java/binary-tree-postorder-traversal/Solution.java
@@ -0,0 +1,40 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+public class Solution {
+
+    public List<Integer> postorderTraversal(TreeNode root) {
+        List<Integer> inorderTraversal = new ArrayList<>();
+        
+        if(root == null)
+            return inorderTraversal;
+
+        inorderTraversal.addAll(postorderTraversal(root.left));
+        inorderTraversal.addAll(postorderTraversal(root.right));
+        inorderTraversal.add(root.val);
+
+        return inorderTraversal;
+    }
+
+    public static void main(String[] args) {
+        TreeNode tree = new TreeNode();
+        tree.val = 1;
+        tree.left = null;
+        tree.right = new TreeNode(2, new TreeNode(3, null, null), null);
+        Solution s = new Solution();
+        System.out.println(s.postorderTraversal(tree));
+    }
+}

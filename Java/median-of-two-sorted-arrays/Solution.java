486378625469cc155867e0030e49d018a9c51bc3
gyurika <gyorgykoszegi39@gmail.com>
2023-09-21 11:14:28 +0200

    finds median of two sorted array - improved solution

--- Java/median-of-two-sorted-arrays/Solution.java
+++ Java/median-of-two-sorted-arrays/Solution.java
@@ -1,6 +1,6 @@
 
 public class Solution {
-    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+    public double findMedianSortedArrays1(int[] nums1, int[] nums2) {
         int n = nums1.length, m = nums2.length, mergedNums[] = new int[n + m];
         int i = 0, j = 0, k = 0;
 
@@ -26,6 +26,34 @@ public class Solution {
             return (double)(mergedNums[mid - 1] + mergedNums[mid]) / 2;
 
         return mergedNums[mid];
+    }
+
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        int n = nums1.length, m = nums2.length;
+        int leftMid = (n + m + 1) / 2, rightMid = (n + m + 2) / 2;
+
+        if(leftMid == rightMid)
+            return findMedian(nums1, 0, nums2, 0, leftMid);
+
+        return (double)(findMedian(nums1, 0, nums2, 0, leftMid) + findMedian(nums1, 0, nums2, 0, rightMid)) / 2;
+    }
+
+    public int findMedian(int[] nums1, int i, int[] nums2, int j, int k) {
+        int n = nums1.length, m = nums2.length;
+        if(i >= n) 
+            return nums2[j + k - 1];
+        if(j >= m) 
+            return nums1[i + k - 1];
+        if(k == 1) 
+            return Math.min(nums1[i], nums2[j]);
+
+        int leftValue = i + k/2 - 1 < n ? nums1[i + k/2 - 1] : Integer.MAX_VALUE;
+        int rightValue = j + k/2 - 1 < m ? nums2[j + k/2 - 1] : Integer.MAX_VALUE;
+
+        if(leftValue < rightValue)
+            return findMedian(nums1, i + k/2, nums2, j, k - k/2); // nums1 right - nums2 left
+        else
+            return findMedian(nums1, i, nums2, j + k/2, k - k/2); // nums1 left - nums2 right
     }
 
     public static void main(String[] args) {

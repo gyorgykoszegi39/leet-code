3de9e8b7c3f766d1c5120b3cc3565a7594c06c27
gyurika <gyorgykoszegi39@gmail.com>
2023-10-02 09:59:52 +0200

    removes unused var

--- Java/remove-colored-pieces-if-both-neighbors-are-the-same-color/Solution.java
+++ Java/remove-colored-pieces-if-both-neighbors-are-the-same-color/Solution.java
@@ -1,19 +1,19 @@
 public class Solution {
     public boolean winnerOfGame(String colors) {
-        int countA = 0, countB = 0;
+        int countA = 0;
         char prevPrevChar = 'N', prevChar = 'N';
         for(char ch : colors.toCharArray()) {
             if(ch == prevChar && ch == prevPrevChar) {
                 if(ch == 'A')
                     countA++;
                 else
-                    countB++;
+                countA--;
             } else {
                 prevPrevChar = prevChar;
                 prevChar = ch;
             }
         }
-        return countA > countB;
+        return countA > 0;
     }
 
     public static void main(String[] args) {

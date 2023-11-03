eb62e4668c9085e62061bfac33fbdf7b3ab49717
gyurika <gyorgykoszegi39@gmail.com>
2023-11-03 11:01:34 +0100

    builds an array with stack operations

---
+++ Java/build-an-array-with-stack-operations/Solution.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution {
+    public List<String> buildArray(int[] target, int n) {
+        List<String> ops = new ArrayList<>();
+        int j = 0, m = target[target.length - 1];
+
+        for(int i = 1; i <= n && j < target.length; i++) {
+            ops.add("Push");
+            if(i == target[j]) {
+                j++;
+            }
+            else {
+                ops.add("Pop");
+            }
+        }
+        return ops;
+    }
+
+    public static void main(String[] args) {
+        Solution s = new Solution();
+
+        for(String op : s.buildArray(new int[] {1, 3}, 4)) {
+            System.out.println(op);
+        }
+    }
+}

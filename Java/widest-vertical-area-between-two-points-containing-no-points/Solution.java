8d1a12104f916fef3262737f740eb952e278adaa
gyurika <gyorgykoszegi39@gmail.com>
2023-12-21 07:46:15 +0100

    finds longest vertical area - 2

--- Java/widest-vertical-area-between-two-points-containing-no-points/Solution.java
+++ Java/widest-vertical-area-between-two-points-containing-no-points/Solution.java
@@ -16,6 +16,15 @@ public class Solution {
         return maxDiff;
     }
 
+    public int maxWidthOfVerticalArea_2(int[][] points) {
+        Arrays.sort(points, (a, b) -> a[0] - b[0]);
+        int maxDiff = 0;
+        for(int i = 1; i < points.length; i ++)
+            maxDiff = Math.max(maxDiff, points[i][0] - points[i - 1][0]);
+
+        return maxDiff;
+    }
+
     public static void main(String[] args) {
         Solution s = new Solution();
 

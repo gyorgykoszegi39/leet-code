cb5f554c62fd91c483cddba1ac29dcbc5f326272
gyurika <gyorgykoszegi39@gmail.com>
2024-01-08 14:49:15 +0100

    reverses integer

--- Java/reverse-integer/Solution.java
+++ Java/reverse-integer/Solution.java
@@ -1,21 +1,15 @@
 public class Solution {
     public int reverse(int x) {
-        boolean isNegative = false;
-        if(x < 0) {
-            isNegative = true;
-            x *= (-1);
-        }
-
         long reversed = 0;
-        while(x > 0) {
+        while(x != 0) {
             reversed = reversed * 10 + x % 10;
             x /= 10;
         }
 
-        if(reversed > Integer.MAX_VALUE)
+        if(reversed < Integer.MIN_VALUE || reversed > Integer.MAX_VALUE)
             return 0;
         
-        return isNegative ? -(int)reversed : (int)reversed;
+        return (int)reversed;
     }
 
     public static void main(String[] args) {

925415392a243d2264ca4bf074febe23a7cadc6c
gyurika <gyorgykoszegi39@gmail.com>
2024-05-28 19:35:25 +0200

    gets equal substrings wihtin budget

--- Java/get-equal-substrings-within-budget/Solution.java
+++ Java/get-equal-substrings-within-budget/Solution.java
@@ -1,14 +1,15 @@
 public class Solution {
 
     public int equalSubstring(String s, String t, int maxCost) {
-        int n = s.length();
+        char[] sChars = s.toCharArray();
+        char[] tChars = t.toCharArray();
         int start = 0, cost = 0, count = 0;
 
         for (int i = 0; i < s.length(); i++) {
-            cost += Math.abs(s.charAt(i) - t.charAt(i));
+            cost += Math.abs(sChars[i] - tChars[i]);
 
             while (cost > maxCost) {
-                cost -= Math.abs(s.charAt(start) - t.charAt(start));
+                cost -= Math.abs(sChars[start] - tChars[start]);
                 start++;
             }
 
@@ -24,5 +25,5 @@ public class Solution {
         System.out.println(s.equalSubstring("abcd", "bcdf", 3));
         System.out.println(s.equalSubstring("krrgw", "zjxss", 19));
 
-    }    
+    }
 }
